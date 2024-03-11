5f1881436e9abbe69b8cfae2e28d6c247fdefa17
João Victor Martins <joaovmf4@gmail.com>
2024-03-11 20:04:50 -0300

    Criação de componentes

---
+++ src/components/Sedans.jsx
@@ -0,0 +1,12 @@
+import iconSedans from "../assets/icon-sedans.svg"
+
+export default function Sedans() {
+  return (
+    <div className="bg-orange p-12 max-lg:rounded-t-lg lg:rounded-l-lg">
+        <img src={iconSedans} alt="ícone dos carros sedan" className="mb-7" />
+        <h2 className="font-shoulders mb-7 uppercase text-4xl font-bold text-off-white-bg">Sedans</h2>
+        <p className="font-lexend mb-7 text-off-white lg:mb-20">Escolha um sedan pelo seu preço acessível e excelente economia de combustível. Ideal para dirigir na cidade ou na sua próxima viagem.</p>
+        <a href="https://www.webmotors.com.br/sedans " className="font-lexend bg-off-white-bg block px-10 py-3 w-fit rounded-3xl text-orange border-2 border-off-white-bg hover:bg-orange hover:text-off-white-bg" target="_blank">Ver mais</a>
+    </div>
+  )
+}
---
+++ src/components/Suvs.jsx
@@ -0,0 +1,12 @@
+import iconSuvs from "../assets/icon-suvs.svg"
+
+export default function Suvs() {
+  return (
+    <div className="bg-green p-12">
+        <img src={iconSuvs} alt="ícone dos carros SUV" className="mb-7"/>
+        <h2 className="font-shoulders mb-7 uppercase text-4xl font-bold text-off-white-bg">SUVs</h2>
+        <p className="font-lexend mb-7 text-off-white lg:mb-20">Escolha um SUV pelo seu interior espaçoso, poder e versatilidade. Perfeito para passar as férias em família ou para aventuras off-road.</p>
+        <a href="https://www.webmotors.com.br/suvs"  className="font-lexend bg-off-white-bg block px-10 py-3 w-fit rounded-3xl text-green border-2 border-off-white-bg hover:bg-green hover:text-off-white-bg" target="_blank">Ver mais</a>
+    </div>
+  )
+}
