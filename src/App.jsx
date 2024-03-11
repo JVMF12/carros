c989209953103ee6337c964806c8e8cf03d3bdcc
João Victor Martins <joaovmf4@gmail.com>
2024-03-11 19:26:50 -0300

    Sexto commit do App

--- src/App.jsx
+++ src/App.jsx
@@ -7,25 +7,25 @@ export default function App() {
     <main className="min-h-svh bg-off-white-bg py-20 px-6">
 
       <section>
-        <div className="bg-orange">
-          <img src={iconSedans} alt="ícone dos carros sedan" />
-          <h2 className="font-shoulders">Sedans</h2>
-          <p className="font-lexend">Escolha um sedan pelo seu preço acessível e excelente economia de combustível. Ideal para dirigir na cidade ou na sua próxima viagem.</p>
+        <div className="bg-orange p-12 rounded-t-lg">
+          <img src={iconSedans} alt="ícone dos carros sedan" className="mb-7" />
+          <h2 className="font-shoulders mb-7 uppercase text-4xl font-bold text-off-white-bg">Sedans</h2>
+          <p className="font-lexend mb-7">Escolha um sedan pelo seu preço acessível e excelente economia de combustível. Ideal para dirigir na cidade ou na sua próxima viagem.</p>
           <a href="https://www.webmotors.com.br/sedans " className="font-lexend" target="_blank">Ver mais</a>
         </div>
 
-        <div className="bg-green">
-            <img src={iconSuvs} alt="ícone dos carros SUV" />
-            <h2 className="font-shoulders">SUVs</h2>
-            <p className="font-lexend">Escolha um SUV pelo seu interior espaçoso, poder e versatilidade. Perfeito para passar as férias em família ou para aventuras off-road.</p>
+        <div className="bg-green p-12">
+            <img src={iconSuvs} alt="ícone dos carros SUV" className="mb-7"/>
+            <h2 className="font-shoulders mb-7 uppercase text-4xl font-bold text-off-white-bg">SUVs</h2>
+            <p className="font-lexend mb-7">Escolha um SUV pelo seu interior espaçoso, poder e versatilidade. Perfeito para passar as férias em família ou para aventuras off-road.</p>
             <a href="https://www.webmotors.com.br/suvs"  className="font-lexend" target="_blank">Ver mais</a>
         </div>
 
            
-        <div className="bg-dark-green">
-            <img src={iconLuxo} alt="" />
-            <h2 className="font-shoulders">Luxuosos</h2>
-            <p className="font-lexend">Passeie nos melhores carros sem os preços exorbitantes. Desfrute do conforto aprimorado de um aluguel de luxo e chegue com estilo</p>
+        <div className="bg-dark-green p-12 rounded-b-lg">
+            <img src={iconLuxo} alt="" className="mb-7" />
+            <h2 className="font-shoulders mb-7 uppercase text-4xl font-bold text-off-white-bg">Luxuosos</h2>
+            <p className="font-lexend mb-7">Passeie nos melhores carros sem os preços exorbitantes. Desfrute do conforto aprimorado de um aluguel de luxo e chegue com estilo</p>
             <a href="https://www.webmotors.com.br/carros-de-luxo" className="font-lexend" target="_blank">Ver mais</a>
         </div>
       </section>
